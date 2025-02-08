0892645e7158e5fa33270d94b7e2d99a2c66d653
wimalopaan <wilhelm.wm.meier@googlemail.com>
2025-02-08 16:09:32 +0100

    added passibility to set virtual inputs via sliders

--- WIDGETS/lvglControls/ui.lua
+++ WIDGETS/lvglControls/ui.lua
@@ -9,16 +9,19 @@ local serialize = loadScript("/WIDGETS/" .. dir .. "/tableser.lua")();
 
 widget.ui = nil;
 
-local settings = {}
+local settings = {};
+local state = {};
 local settingsFilename = "/WIDGETS/" .. dir .. "/" .. model.getInfo().name .. "_" .. widget.options.Name .. ".lua";
 
 local function resetSettings()
     settings.version = settingsVersion;
     settings.numberOfSliders = 6;
     settings.sliders = {};
+    state.values = {};
     for i = 1, 16 do
-        settings.sliders[i] = { name = "VS" .. i, shm = i, width = (LCD_W - 20) / settings.numberOfSliders,
+        settings.sliders[i] = { name = "VS" .. i, shm = i, vin = i, useShm = 0, width = (LCD_W - 20) / settings.numberOfSliders,
         color = COLOR_THEME_SECONDARY3, textColor = COLOR_THEME_PRIMARY3, font = 0 };
+        state.values[i] = 0;
     end
 end
 resetSettings();
@@ -31,11 +34,18 @@ local function createSlider(i)
     return { type = "box", flexFlow = lvgl.FLOW_COLUMN, w = settings.sliders[i].width, children = {
             { type = "label", text = settings.sliders[i].name, color = settings.sliders[i].textColor, font = settings.sliders[i].font},
             { type = "verticalSlider", min = -1024, max = 1024, h = 180,
-                get = (function() return getShmVar(settings.sliders[i].shm) end),
-                set = (function(v) setShmVar(settings.sliders[i].shm, v); end),
+                get = (function() if (settings.sliders[i].useShm > 0) then return getShmVar(settings.sliders[i].shm); else return state.values[i]; end; end),
+                set = (function(v) if (settings.sliders[i].useShm > 0) then 
+                                        setShmVar(settings.sliders[i].shm, v); 
+                                    else 
+                                        state.values[i] = v; 
+                                        setVirtualInput(settings.sliders[i].vin, v); 
+                                        print("set", settings.sliders[i].vin, v);
+                                    end; 
+                                end),
                 color = settings.sliders[i].color
             },
-            { type = "label", text = (function() return math.floor(getShmVar(i) / 10.24 + 0.5) .. "%"; end)}
+            { type = "label", text = (function() if (settings.sliders[i].useShm > 0) then return math.floor(getShmVar(i) / 10.24 + 0.5) .. "%"; else return math.floor(state.values[i] / 10.24 + 0.5) .. "%"; end; end)}
             }
         };
 end
@@ -89,8 +99,19 @@ local function createSetting(i)
             { type = "label", text = " Font:" },
             { type = "font", get = (function() return settings.sliders[i].font; end),
                             set = (function(v) settings.sliders[i].font = v; end) },                                     
+            { type = "label", text = " UseShm:" },
+            { type = "toggle", get = (function() return settings.sliders[i].useShm; end),
+                               set = (function(v) settings.sliders[i].useShm = v; end)},
             {type = "label", text = "ShmV: "},
-            {type = "numberEdit", min = 1 , max = 16, w = 60, get = (function() return settings.sliders[i].shm; end), set = (function(v) settings.sliders[i].shm = v; end) },                             
+            {type = "numberEdit", min = 1 , max = 16, w = 60, 
+                    active = (function() return (settings.sliders[i].useShm > 0); end),
+                    get = (function() return settings.sliders[i].shm; end), 
+                    set = (function(v) settings.sliders[i].shm = v; end) },                             
+            {type = "label", text = "Vin: "},
+            {type = "numberEdit", min = 1 , max = 16, w = 60, 
+                    active = (function() return (settings.sliders[i].useShm == 0); end),
+                    get = (function() return settings.sliders[i].vin; end), 
+                    set = (function(v) settings.sliders[i].vin = v; end) },                             
         }
     }
 end
