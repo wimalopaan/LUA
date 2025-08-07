9043085a13681f5a8eed5e27599edba1de685ff9
wimalopaan <wilhelm.wm.meier@googlemail.com>
2025-08-07 15:09:14 +0200

    added new subproto to swap ch1-16 and ch17-32 in SBus and SumDV3 (needs 3.x.x-wmsumdv3 or 3.x.x-wmchanext)

--- SCRIPTS/MIXES/crsfch.lua
+++ SCRIPTS/MIXES/crsfch.lua
@@ -23,9 +23,8 @@ CRSF_FRAMETYPE_CMD      = 0x32
 CRSF_REALM_CC           = 0xA0;
 CRSF_SUBCMD_CC_ADATA    = 0x01;
 CRSF_SUBCMD_CC_ACHUNK   = 0x02;
-CRSF_SUBCMD_CC_ACHANNEL = 0x03;
-CRSF_SUBCMD_CC_ACHAN_LO = 0x04;
-CRSF_SUBCMD_CC_ACHAN_HI = 0x05;
+CRSF_SUBCMD_CC_ACHANNEL = 0x03; -- 16 channels / 8bit
+CRSF_SUBCMD_CC_ACHAN_EXT= 0x04; -- flag-byte, 16 channels / 8bit 
 
 local function scaleTo8Bit(channel)
   local v = getOutputValue(channel - 1);
@@ -36,25 +35,34 @@ local function scaleTo8Bit(channel)
   end
 end
 
-local function sendChannels(startChannel, numberOfChannels) 
-  print("sendchannels", startChannel, numberOfChannels, "ch:", scaleTo8Bit(startChannel));
-  local payloadOut = {CRSF_ADDRESS_CONTROLLER, CRSF_ADDRESS_TRANSMITTER, CRSF_REALM_CC, CRSF_SUBCMD_CC_ACHANNEL};
-  for ch=startChannel, (startChannel + numberOfChannels - 1) do
-    payloadOut[5 + ch - startChannel] = scaleTo8Bit(ch);
+local function sendChannels(startChannel, numberOfChannels, flags) 
+  print("sendchannels", flags, startChannel, numberOfChannels, "ch:", scaleTo8Bit(startChannel));
+  if (flags > 0) then
+    local payloadOut = {CRSF_ADDRESS_CONTROLLER, CRSF_ADDRESS_TRANSMITTER, CRSF_REALM_CC, CRSF_SUBCMD_CC_ACHAN_EXT, flags};
+    for ch=startChannel, (startChannel + numberOfChannels - 1) do
+      payloadOut[6 + ch - startChannel] = scaleTo8Bit(ch);
+    end
+    crossfireTelemetryPush(CRSF_FRAMETYPE_CMD, payloadOut);
+  else
+    local payloadOut = {CRSF_ADDRESS_CONTROLLER, CRSF_ADDRESS_TRANSMITTER, CRSF_REALM_CC, CRSF_SUBCMD_CC_ACHANNEL};
+    for ch=startChannel, (startChannel + numberOfChannels - 1) do
+      payloadOut[5 + ch - startChannel] = scaleTo8Bit(ch);
+    end
+    crossfireTelemetryPush(CRSF_FRAMETYPE_CMD, payloadOut);
   end
-  crossfireTelemetryPush(CRSF_FRAMETYPE_CMD, payloadOut);
 end
 
 local inputs = {
 	{"StartC", VALUE, 1, 17, 17},
-  {"Interv", VALUE, 1, 99, 5}
+  {"Interv", VALUE, 1, 99, 5},
+  {"Flags", VALUE, 0, 3, 0}
 }
 
 local last = getTime();
-local function run(startChannel, Interv)
+local function run(startChannel, Interv, flags)
     local t = getTime();
     if ((t - last) >= Interv) then
-      sendChannels(startChannel, 16);
+      sendChannels(startChannel, 16, flags);
       last = t;
     end  
 end
